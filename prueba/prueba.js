597aeef7a33ebf99eb53f5f6214537b51ae680b9
Ceinneccol <inneccocarlose80@gmail.com>
2022-06-21 00:24:06 +0200

    funciones js

---
+++ prueba/prueba.js
@@ -0,0 +1,20 @@
+// console.log("hola me gusta la marihuana")
+
+const value = 5;
+let value1 = "carlos eduardo";
+
+function sumar() {
+    return value + 1;
+}
+
+function sumarTodo(){
+    let sumar = sumar();
+    return sumar + 1;
+}
+
+// console.log(sumarTodo())
+
+sumar();
+
+const myarray = [1,10,5]
+console.log(myarray.length)
