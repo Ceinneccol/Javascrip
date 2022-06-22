2212ffd494c4a59fc84f8371e3ea082143361378
Ceinneccol <inneccocarlose80@gmail.com>
2022-06-22 23:53:44 +0200

    carpeta nueva

---
+++ cursoudemy/variables.js
@@ -0,0 +1,14 @@
+var saludo = "Hola Carlos"
+if(saludo){
+    var saludo = "HOLA"
+    console.log(saludo)
+}
+console.log(saludo)
+let nombre = "Carlos"
+if(nombre){
+    let nombre = "Eduardo"
+    console.log(nombre)
+}
+console.log(nombre)
+const PI = 3.14
+console.log(PI)
