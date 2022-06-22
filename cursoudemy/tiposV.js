9ad1109e6c8e0694b56d0f3df422563254c98ce4
Ceinneccol <inneccocarlose80@gmail.com>
2022-06-23 00:36:14 +0200

    carpeta nueva

---
+++ cursoudemy/tiposV.js
@@ -0,0 +1,15 @@
+var nombre = 'Carlos' //variable string (texto)
+console.log(nombre)
+var miNumero = 42 //variable number (numero)
+console.log(miNumero)
+const PI = 3.14 //variable decimal (constante)
+console.log(PI)
+var verdadero = true //variable boolean true
+var falso = false //variable boolean false
+console.log(verdadero)
+console.log(falso)
+var nulo = null //variable null
+console.log(nulo)
+var indefinido = undefined //variable indefined (indefinido sin valor)
+console.log(indefinido)
+
